0ffd8b4545b1ac461770cfc0198aee8289313984
kazuyuki-hosoda <kazuyuki.hosoda@asia-quest.jp>
2022-11-09 10:16:47 +0900

    useRouterを使った画面遷移

--- src/pages/router/[name]/setting.js
+++ src/pages/router/[name]/setting.js
@@ -2,7 +2,25 @@ import { useRouter } from "next/router";
 
 export default function Setting({ query }) {
     const router = useRouter();
-    return <h1>{router.query.name}</h1>
+    const clickhandler = () => {
+        router.push('/');
+    }
+    const clickhandler2 = () => {
+        router.replace('/');
+    }
+    const clickhandler3 = () => {
+        router.back();
+    }
+    const clickhandler4 = () => {
+        router.reload();
+    }
+  
+    return (
+        <>
+            <h1>{router.query.name}</h1>
+            <button onclick={clickhandler}>画面遷移</button>
+        </>
+    )
 }
 
 export async function getServerSideProps({ query }) {
