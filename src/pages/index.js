d272cf468a2994dce72caabf15778020c382046a
kazuyuki-hosoda <kazuyuki.hosoda@asia-quest.jp>
2022-11-09 10:23:54 +0900

    Linkを使った画面遷移

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,15 @@
+import Link from "next/link";
 
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <h1>Home</h1>
+      <Link href={{ pathname: "/router",query: { key: "value" }}}>
+        <a>/router</a>
+      </Link>
+      <Link href="/router/hello/setting" as="/dummy-url">
+        <a>/router</a>
+      </Link>
+    </>
+  )
 }
